1d399df7c247f26fdbfa346ab9ed8ab464440ef9
droid-lover <ersachin20@gmail.com>
2022-12-29 14:18:12 +0530

    Defined initial dependencies using versions file

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,10 @@
+object Dependencies {
+
+    val coreKtx by lazy { "androidx.core:core-ktx:${Versions.coreKtx}" }
+    val appcompat by lazy { "androidx.appcompat:appcompat:${Versions.appcompat}" }
+    val material by lazy { "com.google.android.material:material:${Versions.material}" }
+    val constraintLayout by lazy { "androidx.constraintlayout:constraintlayout:${Versions.constraintlayout}" }
+    val junit by lazy { "junit:junit:${Versions.junit}" }
+    val extJunit by lazy { "androidx.test.ext:junit:${Versions.extJunit}" }
+    val espressoCore by lazy { "androidx.test.espresso:espresso-core:${Versions.espressoCore}" }
+}
