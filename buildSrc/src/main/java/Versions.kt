d458de092b030d0a2e1725b1ab554833f4d5691d
droid-lover <ersachin20@gmail.com>
2022-12-29 14:17:47 +0530

    Defined initial versions

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,13 @@
+object Versions {
+
+    val coreKtx = "1.7.0"
+    val appcompat = "1.5.1"
+    val material = "1.7.0"
+    val constraintlayout = "2.1.4"
+    val junit = "4.13.2"
+    val extJunit = "1.1.3"
+    val espressoCore = "3.4.0"
+
+
+}
+
